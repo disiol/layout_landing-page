20a0141c8266310df3b969d7c2804696f00d1033
denisimus <deoniisii@gmail.cim>
2025-02-06 03:44:40 +0200

    setup DEMO LINK

--- cypress/integration/page.spec.js
+++ cypress/integration/page.spec.js
@@ -1,6 +1,8 @@
 'use strict';
 
 // TODO: edit it
+// eslint-disable-next-line no-undef
 describe('Page', () => {
+  // eslint-disable-next-line no-undef
   it('should be visitable', () => {});
 });
